433a299d25ae2c13a444808e0a58265da1d11eca
masayuki <hcsabc2019@gmail.com>
2022-05-23 14:43:51 +0900

    2022/05/23 ユーザー作成途中

--- src/main/java/com/example/diaryProject/domain/user/UserRepository.java
+++ src/main/java/com/example/diaryProject/domain/user/UserRepository.java
@@ -2,11 +2,17 @@ package com.example.diaryProject.domain.user;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Optional;
 
 @Mapper
 public interface UserRepository {
 
+    @Select("select * from users where name = #{name}")
+    Optional<User> findByUsername(String username);
 
     @Insert("insert into users (name,password,authority) values(#{name},#{pw},#{authority})")
     void create(String name, String pw, String authority);
+
 }
--- src/main/java/com/example/diaryProject/domain/user/UserService.java
+++ src/main/java/com/example/diaryProject/domain/user/UserService.java
@@ -1,14 +1,18 @@
 package com.example.diaryProject.domain.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
 
     public void create(String name,String pw,String authority){
-        userRepository.create(name,pw,authority);
+       var encodPassword =  passwordEncoder.encode(pw);
+        userRepository.create(name,encodPassword,authority);
     }
 }
